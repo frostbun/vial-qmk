161f7959a1fd4dbeb1f7724eb3dfec3dc7138db3
Manh Nguyen <manhnb1911@gmail.com>
2023-04-09 16:09:24 +0700

    add right hand unlock combo for macaron split

--- keyboards/frostbun/macaron_split/keymaps/vial/config.h
+++ keyboards/frostbun/macaron_split/keymaps/vial/config.h
@@ -4,5 +4,6 @@
 #pragma once
 
 #define VIAL_KEYBOARD_UID {0xE3, 0x41, 0x21, 0xB8, 0x42, 0x64, 0xE8, 0x95}
+// #define VIAL_UNLOCK_COMBO_ROWS {4, 5}
 #define VIAL_UNLOCK_COMBO_ROWS {0, 1}
 #define VIAL_UNLOCK_COMBO_COLS {0, 1}
